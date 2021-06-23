6f52cba7819cc798097c91f0f6c60901dbec803d
Swapnil More <swapnilmore@iisc.ac.in>
2021-06-23 22:41:46 +0530

    Updating statuses with sockets

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 users = ['00','01','10','11']
 active = ['no','no','no','no']
@@ -7,34 +9,39 @@ optMsg = ['its so cold in here!','peace','peace','this heat is killing me!']
 
 usermap = {v:active[i] for i,v in enumerate(users)}
 
-
 @app.route('/')
 def index():
-    # print(list(usermap.values()))
     return render_template('index.html', users=users, active=active, optMsg = optMsg)
-
-
-@app.route('/update/<user>/<state>', methods = ['GET', 'POST'])
-def update_state(user,state):
-    global usermap, users, active
-    ind = users.index(user)
-    active[ind] = state
-    usermap[user] = state
-    # print(usermap)
-    return str(user) + ' active ' + str(state)
-
 
 @app.route('/update_status/<user>/<status>', methods = ['GET', 'POST'])
 def update_status(user,status):
     global optMsg
     ind = users.index(user)
     optMsg[ind] = status
-    print('==============')
-    print("status" + users[ind] + ' to ' + optMsg[ind])
-    print('==============')
-
     return "status" + users[ind] + ' to ' + optMsg[ind]
 
+@socketio.on("tap in")
+def tap_in(data):
+    global usermap, active
+    ind = users.index(data["card"])
+    active[ind] = 'yes'
+    usermap[data["card"]] = 'yes'
+    emit("announce tap in", {"card":data["card"]}, broadcast = True)
+
+@socketio.on("tap out")
+def tap_in(data):
+    global usermap, active
+    ind = users.index(data["card"])
+    active[ind] = 'no'
+    usermap[data["card"]] = 'no'
+    emit("announce tap out", {"card":data["card"]}, broadcast = True)
+
+@socketio.on("status change")
+def status_change(data):
+    global optMsg
+    ind = users.index(data["card"])
+    optMsg[ind] = data["status"]
+    emit("announce status change", {"card":data["card"],"status":data["status"]}, broadcast = True)
 
 if __name__ == '__main__':
   app.run(host='', port=8000, debug=True)
--- static/assets/js/bs-init.js
+++ static/assets/js/bs-init.js
@@ -1,169 +1,126 @@
 var optMsg = {'00':'its so cold in here!','01':'peace','10':'peace','11':'this heat is killing me!'};
+var optMsgColor = {'00':'#39a2db','01':'#ffface','10':'#ffface','11':'#ff616d'};
 var cards;
 var sessionUser;
 var statusNode;
 sessionUser = localStorage.getItem("sessionUser");
 
-function sleep(miliseconds) {
-   let currentTime = new Date().getTime();
-
-   while (currentTime + miliseconds >= new Date().getTime()) {
-   }
-}
-
-
 document.addEventListener("DOMContentLoaded", ()=>{
 
-  statusNode = document.getElementById('status');
-  statusNode.hidden = true;
-  try {
-    document.getElementById('charc-'+sessionUser).innerHTML = localStorage.getItem("statusMessage")
-  } catch (e) {
-    console.log(e);
-  };
+  // SocketIO Connection
+  updateCountLabel();
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+  socket.on("connect", ()=>{
+    console.log("socket connected");
 
-  cards = document.getElementsByClassName("card");
-	for (var card of cards) {
-		card.addEventListener("click", (event) => {
-			let target = event.target || event.srcElement;
-			let clsList = target.classList;
-			let user, charac;
-			clsList.forEach((item, i) => {
-				if (item.startsWith("user")) {
-					user = item.split("-")[1];
-				}
-			});
+    if (sessionUser != null) {
+      socket.emit('tap in', {'card': sessionUser});
+    }
 
-      let characEl = document.getElementById('charc-'+user);
+    statusNode = document.getElementById('status');
+    statusNode.hidden = true;
+    try {
+      document.getElementById('charc-'+sessionUser).innerHTML = localStorage.getItem("statusMessage")
+    } catch (e) {
+      console.log("no session user yet");
+    };
 
-      switch (user) {
-        case '00':
-          charc = "its so cold in here!";
-          characEl.style.color = "#39a2db";
-          break;
-        case '11':
-          charc = "this heat is killing me!";
-          characEl.style.color = "#ff616d";
+    cards = document.getElementsByClassName("card");
+  	for (var card of cards) {
+  		card.addEventListener("click", (event) => {
+  			let target = event.target || event.srcElement;
+  			let clsList = target.classList;
+  			let user, charac;
 
-          break;
-        case '01':
-          charc = "peace";
-          characEl.style.color = "#ffface";
+  			clsList.forEach((item, i) => {
+  				if (item.startsWith("user")) {
+  					user = item.split("-")[1];
+  				}
+  			});
 
-          break;
-        case '10':
-          charc = "peace";
-          characEl.style.color = "#ffface";
+  			let active;
 
-          break;
-        default:
-          charc = "";
-      }
+        document.getElementById('card-'+user).classList.forEach((item, i) => {
+  				if (item.startsWith("active")) {
+  					active = item.split("-")[1];
+  				}
+  			});
 
-			let user_card = document.getElementById(user)
-			let active;
-			user_card.classList.forEach((item, i) => {
-				if (item.startsWith("active")) {
-					active = item.split("-")[1];
-				}
-			});
-      let subtitle = user_card.childNodes[1].childNodes[3]
-
-			if (active == "yes" ) {
-        statusNode.hidden = true;
-        if (sessionUser == user) {
-          localStorage.clear();
-          user_card.classList.remove("active-yes");
-  				user_card.classList.add("active-no");
-  				user_card.style.animationName = "slideout";
-  				user_card.style.animationDuration = "1s";
-  				sleep(200)
-  				user_card.style.backgroundColor = "rgb(12,8,27)";
-  				// user_card.parentNode.style.order = 1;
-          subtitle.innerHTML = "is available"
-          // update state on serve
-
-          characEl.innerHTML = "";
-
-          let sendPost = async () => {
-            let status = 'no';
-            let url = `/update/${user}/${status}`; // the URL to send the HTTP request to
-            let method = 'POST';
-            let response = await fetch(url, { method });
-            let data = await response.text(); // or response.json() if your server returns JSON
-
+  			if (active == "yes" ) {
+          statusNode.hidden = true;
+          if (sessionUser == user) {
+            localStorage.clear();
+            socket.emit('status change', {'card': user, 'status':optMsg[user]});
+            socket.emit('tap out', {'card': user});
           }
 
-          sendPost();
+  			} else {
 
-
-          let sendPost_reset_status = async () => {
-            let status = 'yes';
-            let sm = optMsg[user];
-            console.log("---------------");
-            console.log(sm);
-            console.log("---------------");
-
-            let url = `/update_status/${user}/${sm}`; // the URL to send the HTTP request to
-            let method = 'POST';
-            let response = await fetch(url, { method });
-            let data = await response.text(); // or response.json() if your server returns JSON
-            console.log(data);
+          if (localStorage.getItem("sessionUser") == null) {
+            localStorage.setItem("sessionUser", user);
+            sessionUser = localStorage.getItem("sessionUser");
+            statusNode.hidden = false;
+            socket.emit('tap in', {'card': user});
           }
 
-          sendPost_reset_status();
-        }
-        else {
-          console.log("you are not the session user for this card");
-        }
+  			}
 
-        //
+  		})
+  	}
 
-			} else {
+    statusNode.onsubmit = (e) => {
+      e.preventDefault();
+      statusNode.hidden = true;
+      let statusMessage = statusNode["statusMessage"].value;
+      statusNode.reset();
+      socket.emit('status change', {'card': sessionUser, 'status':statusMessage});
+    }
 
+  }) // socket on connect exit
 
-        if (localStorage.getItem("sessionUser") == null) {
-          statusNode.hidden = false;
-          localStorage.setItem("sessionUser", user);
-          sessionUser = localStorage.getItem("sessionUser");
-  				user_card.classList.remove("active-no");
-  				user_card.classList.add("active-yes");
-  				user_card.style.animationName = "slidein";
-  				user_card.style.animationDuration = "1s";
-  				sleep(200)
-  				user_card.style.backgroundColor = "#66de93";
-  				// user_card.parentNode.style.order = -1;
-          subtitle.innerHTML = "is occupied"
+  socket.on("announce tap in", data => {
+    let tapInCard = document.getElementById('card-'+data["card"]);
+    let tapInCardSubtitle = document.getElementById('subtitle-'+data["card"]);
+    tapInCardSubtitle.innerHTML = "is occupied"
 
-          characEl.innerHTML = charc;
+    activaeCard(tapInCard);
 
-          let sendPost = async () => {
-            let status = 'yes';
-            let url = `/update/${user}/${status}`; // the URL to send the HTTP request to
-            let method = 'POST';
-            let response = await fetch(url, { method });
-            let data = await response.text(); // or response.json() if your server returns JSON
+    let characEl = document.getElementById('charc-'+data["card"]);
 
-          }
+    characEl.innerHTML = optMsg[data["card"]];
+    characEl.style.color = optMsgColor[data["card"]];
+    updateCountLabel()
 
-          sendPost();
+  })
 
-        }
+  socket.on("announce tap out", data => {
+    let tapOutCard = document.getElementById('card-'+data["card"]);
+    let tapOutCardSubtitle = document.getElementById('subtitle-'+data["card"]);
+    tapOutCardSubtitle.innerHTML = "is available"
 
-        // update state on serve
+    deActivaeCard(tapOutCard)
 
+    let characEl = document.getElementById('charc-'+data["card"]);
 
-        //
-			}
+    characEl.innerHTML = "";
+    characEl.style.color = optMsgColor[data["card"]];
+    updateCountLabel()
 
-		})
-	}
+  })
 
-  let coutLabel = document.getElementById('state_count');
-  let stateCount;
+  socket.on("announce status change", data => {
+    let statusMessage = data["status"];
+    let characEl = document.getElementById('charc-'+data["card"]);
+    localStorage.setItem("statusMessage", statusMessage)
+    characEl.innerHTML = statusMessage;
+    updateCountLabel()
+
+  })
+
   function updateCountLabel() {
-    stateCount = document.getElementsByClassName('active-no').length;
+    let coutLabel = document.getElementById('state_count');
+    let stateCount = document.getElementsByClassName('active-no').length;
     if (stateCount) {
       let isare = (stateCount === 1) ? 'is' : 'are';
       let s = (stateCount === 1) ? '' : 's';
@@ -177,34 +134,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   }
 
-  statusNode["statusMessage"].onfocus = () => {
-    console.log("typing");
-    window.stop()}
-
-  statusNode.onsubmit = (e) => {
-    e.preventDefault();
-    console.log("form submit event");
-    let statusMessage = statusNode["statusMessage"].value;
-    let characEl = document.getElementById('charc-'+sessionUser);
-    localStorage.setItem("statusMessage", statusMessage)
-    characEl.innerHTML = statusMessage;
-    console.log(characEl.innerHTML);
-    let sendPost = async () => {
-      let status = 'yes';
-      let url = `/update_status/${sessionUser}/${statusMessage}`; // the URL to send the HTTP request to
-      let method = 'POST';
-      let response = await fetch(url, { method });
-      let data = await response.text(); // or response.json() if your server returns JSON
-
-    }
-
-    sendPost();
-    statusNode.reset();
-    window.location.reload();
-
+  function activaeCard(cardNode) {
+    cardNode.classList.remove("active-no");
+    cardNode.classList.add("active-yes");
+    cardNode.style.animationName = "slidein";
+    cardNode.style.animationDuration = "1s";
+    cardNode.style.backgroundColor = "#66de93";
   }
 
-	updateCountLabel()
+  function deActivaeCard(cardNode) {
+    cardNode.classList.remove("active-yes");
+    cardNode.classList.add("active-no");
+    cardNode.style.animationName = "slideout";
+    cardNode.style.animationDuration = "1s";
+    cardNode.style.backgroundColor = "rgb(12,8,27)";
 
-  setInterval(updateCountLabel, 200);
+  }
 })
